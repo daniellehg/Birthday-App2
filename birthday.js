92fa4bc1ef9ecff62cff427969ce57a079ecdc2f
Zachary B Hatch <zh86549@srcs.loc>
2018-02-09 10:53:55 -0800

    ghgr

---
+++ birthday.js
@@ -0,0 +1,11 @@
+var button1 = document.getElementById('GetMyAge');
+
+function Birthday() {
+    this.birthdate = new Date(birthdate);
+    var current = new Date()
+    age = Math.round((current - this.birthdate) / 31536000000)
+    return age
+}
+function displayAge(){
+    var input = document.getElementById()
+}
