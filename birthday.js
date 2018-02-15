dd35f7e844a7f5ca68e5106ce81308d221fddea4
daniellehg <daniellehgamble@gmail.com>
2018-02-15 09:18:49 -0800

    attempt to call from the text box- incomplete

--- birthday.js
+++ birthday.js
@@ -19,13 +19,14 @@ function daysTillBirthday() {
     var month = Math.floor((birthdate.getTime() - today.getTime()) / 2.628e+9);
     var hour = Math.floor((birthdate.getTime() - today.getTime()) / 3.6e+6);
     var minute = Math.floor((birthdate.getTime() - today.getTime()) / 60000);
-
+    var result = "There are ", month, " Months, ", days, " Days, ", hour, " hours and ", minute, " minutes until summer!";
+    return result
 }
 
 function displayDays() {
-    var input = document.getElementById('birthdate').value;
-    var result = daysTillBirthday(input);
-    document.getElementById('Days').textContent = "There are ", month, " Months, ", days, " Days, ", hour, " hours and ", minute, " minutes until summer!";
+   // var input = document.getElementById('birthdate').value;
+  //  var result = daysTillBirthday(input);
+    document.getElementById('Days').textContent = "hi";
 
 }
 button3.addEventListener('click', displayDays);
