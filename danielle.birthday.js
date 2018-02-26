824f51487a92142ad4a4d9b0d98e98d3a55db0c3
Danielle H Gamble <dg12181@srcs.loc>
2018-02-26 11:35:05 -0800

    new html page

--- danielle.birthday.js
+++ danielle.birthday.js
@@ -7,23 +7,8 @@ function numberOfdaysUnitlBirthday() {
     var birthdate = new Date(input);
     var today = new Date();
     var days = Math.floor((birthdate.getTime() - today.getTime()) / 86400000); //round down to the nearest day
-
-    var month = Math.floor((birthdate.getTime() - today.getTime()) / 2.628e+9);
-    var hour = Math.floor((birthdate.getTime() - today.getTime()) / 3.6e+6);
-    var minute = Math.floor((birthdate.getTime() - today.getTime()) / 60000);
-    var result = "There are ",
-        month, " Months, ", days, " Days, ", hour, " hours and ", minute, " minutes until summer!";
-    return result
-}
-
-function displayDays() {
-    // var input = document.getElementById('birthdate').value;
-    //  var result = daysTillBirthday(input);
-    document.getElementById('Days').textContent = "hi";
-
     document.getElementById('days').textContent = "Number of days until your birthday: " + days;
 }
-
 
 
 button3.addEventListener('click', numberOfdaysUnitlBirthday);
