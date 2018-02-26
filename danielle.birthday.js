339f5796124dedfa4880208bbbf37749398cc11e
Danielle H Gamble <dg12181@srcs.loc>
2018-02-26 11:51:05 -0800

    revisions to html

--- danielle.birthday.js
+++ danielle.birthday.js
@@ -2,8 +2,7 @@
 var button3 = document.getElementById('getDays');
 
 function numberOfdaysUnitlBirthday() {
-    var input = document.getElementById('birthdate').value
-
+    var input = document.getElementById('day').value
     var birthdate = new Date(input);
     var today = new Date();
     var days = Math.floor((birthdate.getTime() - today.getTime()) / 86400000); //round down to the nearest day
