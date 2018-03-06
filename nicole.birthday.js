cf2b478cc8a072164e0ca9e815c2faf3c814bf38
Nicole  Lescano <nl77656@srcs.loc>
2018-03-06 12:06:31 -0800

    finishing touch

--- nicole.birthday.js
+++ nicole.birthday.js
@@ -1,55 +1,41 @@
-
 var button2 = document.getElementById('findAge');
+button2.addEventListener('click', () => {
+    document.getElementById("Sign").textContent = getSign()
+});
 
 function getSign() {
-    birthday = document.getElementById('inputDate').value;
-    birth = new Date(birthday);
-    var month = birth.getMonth();
+    var input = document.getElementById("inputDate").value;
+    var birth = new Date(input);
+    var month = birth.getMonth() + 1;
     var day = birth.getDay();
     var zodiac = "";
 
-    if ((month() === 0 && day() > 19) || (month === 1 && day < 19)) {
-        zodiac = "Aquarius";
-    } else if ((month() === 1 && day() > 18) || (month() === 2 && day() < 21)) {
-        zodiac = "Pisces";
-    } else if ((month() === 2 && day() > 20) || (month() === 3 && day() < 20)) {
-        zodiac = "Aries";
-    } else if ((month() === 3 && day() > 19) || (month() === 4 && day() < 21)) {
-        zodiac = "Taurus";
-    } else if ((month() === 4 && day() > 20) || (month() === 5 && day() < 21)) {
-        zodiac = "Gemini";
-    } else if ((month() === 5 && day() > 20) || (month() === 6 && day() < 23)) {
-        zodiac = "Cancer";
-    } else if ((month() === 6 && day() > 22) || (month() === 7 && day() < 23)) {
-        zodiac = "Leo";
-    } else if ((month() === 7 && day() > 22) || (month() === 8 && day() < 23)) {
-        zodiac = "Virgo";
-    } else if ((month() === 8 && day() > 22) || (month() === 9 && day() < 23)) {
-        zodiac = "Libra";
-    } else if ((month() === 9 && day() > 22) || (month() === 10 && day() < 22)) {
-        zodiac = "Scorpio";
-    } else if ((month() === 10 && day() > 21) || (month() === 11 && day() < 22)) {
-        zodiac = "Sagittarius";
-    } else if ((month() === 11 && day() > 21) || (month() === 0 && day() < 20)) {
-        zodiac = "Capricorn";
+
+    if ((month == 1 && day >= 19) || (month == 12 && day <= 22)) {
+        var zodiac = "Capricorn";
+    } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+        var zodiac = "Aquarius";
+    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        var zodiac = "Pisces";
+    } else if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+        var zodiac = "Aries";
+    } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+        var zodiac = "Taurus";
+    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        var zodiac = "Gemini";
+    } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+        var zodiac = "Cancer";
+    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+        var zodiac = "Leo";
+    } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+        var zodiac = "Virgo";
+    } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+        var zodiac = "Libra";
+    } else if ((month == 10 && day >= 23) || (month == 11 && day <= 22)) {
+        var zodiac = "Scorpio";
+    } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+        var zodiac = "Sagittarius";
     }
 
-return zodiac ;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+return zodiac;
+};
