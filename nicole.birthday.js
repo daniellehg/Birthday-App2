d6899418a5eb4dc60deaa7d62e14e8695634800c
Nicole  Lescano <nl77656@srcs.loc>
2018-03-06 11:30:01 -0800

    trying to fix

--- nicole.birthday.js
+++ nicole.birthday.js
@@ -1,63 +1,5 @@
-<<<<<<< HEAD
-var button2 = document.getElementById('findAge');
-=======
-<<<<<<< HEAD
-var button2 = document.getElementById('GetMySign');
-button.addEventListener('click', displayGetSign);var button2 = document.getElementById('findSign');
-button2.addEventListener('click', () => {
-    document.getElementById('output').textContent = getSign()
-});
-=======
-
-var button = document.getElementById('findAge');
-
->>>>>>> origin/master
-
-/*function getSign() {
-    var birthday = new Date(birthday);
-    var month = birthday.getMonth() + 1;
-    var day = birthday.getDay();
-
-
-         if ((month == 1 && day <= 19) || (month == 12 && day >= 22)) {
-           return "capricorn";
-        } else if ((month == 1 && day <= 20) || (month == 2 && day >= 18)) {
-            return "aquarius";
-        } else if ((month == 2 && day <= 19) || (month == 3 && day >= 20)) {
-            return "pisces";
-        } else if ((month == 3 && day <= 21) || (month == 4 && day >= 19)) {
-            return "aries";
-        } else if ((month == 4 && day <= 20) || (month == 5 && day >= 20)) {
-            return "taurus";
-        } else if ((month == 5 && day <= 21) || (month == 6 && day >= 20)) {
-            return "gemini";
-        } else if ((month == 6 && day <= 21) || (month == 7 && day >= 22)) {
-            return "cancer";
-        } else if ((month == 7 && day <= 23) || (month == 8 && day >= 22)) {
-            return "leo";
-        } else if ((month == 8 && day <= 23) || (month == 9 && day >= 22)) {
-            return "virgo";
-        } else if ((month == 9 && day <= 23) || (month == 10 && day >= 22)) {
-            return "libra";
-        } else if ((month == 10 && day <= 23) || (month == 11 && day >= 22)) {
-            return "scorpio";
-        } else if ((month == 11 && day <= 22) || (month == 12 && day >= 21)) {
-            return "sagittarius";
-        }
-
-    }
-<<<<<<< HEAD
-=======
-document.getElementById('day').textContent = getSign
-button2.addEventListener('click', getSign);
-<<<<<<< HEAD
-*/
-//var button2 = document.getElementById('findAge');
-=======
 
 var button2 = document.getElementById('findAge');
->>>>>>> origin/master
->>>>>>> origin/master
 
 function getSign() {
     birthday = document.getElementById('inputDate').value;
@@ -91,33 +33,14 @@ function getSign() {
     } else if ((month() === 11 && day() > 21) || (month() === 0 && day() < 20)) {
         zodiac = "Capricorn";
     }
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-return zodiac;
-   /* document.getElementById('Sign').textContent = "Your zodiac sign is" + zodiac; */
-=======
->>>>>>> origin/master
-
-    document.getElementById('Sign').textContent = "Your zodiac sign is" + zodiac;
->>>>>>> origin/master
+return zodiac ;
 }
 
-<<<<<<< HEAD
-function checkFormat() {
-    var format = document.getElementById("birthday").value;
-    var check1 = format.substring(2,3);
-    var check2 = format.substring(5,6);
-    if ((check1 == "/") && (check2 == "/") && (format.length == 10)) {
-        var birhtday = new Date(format);
 
 
 
 
-
-    }
->>>>>>> origin/master
 
 
 
